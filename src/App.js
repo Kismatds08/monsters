52c242e69c008774f32c08115459110bf0842e91
Kismat Shrestha <kismatds08@gmail.com>
2020-12-12 16:44:56 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends React.Component{
   constructor(){
     super()
     this.state = {
-
+      monsters: [],
+      searchField: '',
       }
     }
   render(){
     return(
       <div>
         <h1>Monster's Rollodex</h1>
-        <input type='search' placeholder='Search Monsters'/>
+       <SearchBox/>
         < div className='card-list'>
           <div className='card-container'>
             <img alt='monster' src='https://robohash.org/1?set=set2&size=180x180'/>
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './search-box.styles.css'
+
+export const SearchBox = () => (
+    <input 
+    type ='search'
+    placeholder='Search Monsters'
+    />
+)
+
